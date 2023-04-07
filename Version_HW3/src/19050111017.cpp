8e63b035c7d815f5fd0e489414e95ef9cb480cc8
Semih Gür <semihgur201@gmail.com>
2023-04-07 16:32:05 +0300

    MPI Implementation v0
    trying almost randomly :D

--- Version_HW3/src/19050111017.cpp
+++ Version_HW3/src/19050111017.cpp
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <mpi.h>
 
 int main(int argc, char *argv[])
 {
+  int num_procs, my_rank;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
   // Check if the number of arguments is correct
   if (argc != 4)
   {
@@ -12,17 +19,17 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-  // Convert the first two arguments to integers
-  const int rows = atoi(argv[1]);
-  const int cols = atoi(argv[2]);
+  // Big Matrix 100 + 19050111017 % 100
+  const int rows = 19050111017 % 10000;
+  const int cols = 19050111017 % 10000;
 
   // creating row * col random matrix
-  double** matrix =randomMatrixCreator(rows, cols);
+  double **matrix = randomMatrixCreator(rows, cols);
 
   // creating row * 1 random vector
-  double ** vector = randomMatrixCreator(cols, 1);
+  double **vector = randomMatrixCreator(cols, 1);
 
-  //printing the matrix and vector
+  // printing the matrix and vector
   printf("Matrix is:\n");
   matrixPrinter(matrix, rows, cols);
 
@@ -32,8 +39,12 @@ int main(int argc, char *argv[])
   // Allocate memory for the result vector `solution`
   double *solution = new double[rows];
 
+  // Compute the number of rows to be processed by each process
+  const int rows_per_proc = rows / num_procs;
+  const int remainder_rows = rows % num_procs;
+
   // Perform matrix-vector multiplication and store the result in `solution`
-  for (int i = 0; i < rows; i++)
+  for (int i = my_rank * rows_per_proc; i < my_rank * rows_per_proc + rows_per_proc; i++)
   {
     solution[i] = 0.0;
     for (int j = 0; j < cols; j++)
@@ -42,7 +53,24 @@ int main(int argc, char *argv[])
     }
   }
 
-  std::string filename="../bin/"+std::string(argv[3]);
+  if (my_rank == num_procs - 1)
+  {
+    for (int i = rows - remainder_rows; i < rows; i++)
+    {
+      solution[i] = 0.0;
+      for (int j = 0; j < cols; j++)
+      {
+        solution[i] += matrix[i][j] * vector[j][0];
+      }
+    }
+  }
+  double *final_solution = NULL;
+  if (my_rank == 0) {
+    final_solution = new double[rows];
+  }
+  
+
+  std::string filename = "../bin/" + std::string(argv[3]);
   printf("\nfilename is: %s\n", filename.c_str());
 
   // Write solution to file
